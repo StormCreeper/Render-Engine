49aa48b2f7233a129b195b8e560741daf5961a95
Telo <telo.philippe@telecom-paris.fr>
2024-09-18 17:54:27 +0200

    Simple diffuse lighting

--- shaders/main/fragment.glsl
+++ shaders/main/fragment.glsl
@@ -5,5 +5,16 @@ in vec3 vertexColor;
 in vec3 vertexNormal;
 
 void main() {
-    FragColor = vec4(vertexColor * vertexNormal, 1.0);
+    vec3 objColor = vertexColor;
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * objColor;
+
+    vec3 lightColor = vec3(1.0, 1.0, 0.9);
+    vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
+    float diff = max(dot(vertexNormal, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor * 0.9;
+
+    vec3 result = (ambient + diffuse) * objColor;
+
+    FragColor = vec4(result, 1.0);
 } 
